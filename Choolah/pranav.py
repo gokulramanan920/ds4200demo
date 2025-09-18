6e9a77b6595d8d2ed6a0d19c04b62a602c4d1a80
ramanang711 <ramanang711@gmail.com>
2025-09-18 10:05:43 -0400

    Made a Choolah directory and created a py file that find the total bill of choolah items

---
+++ Choolah/pranav.py
@@ -0,0 +1,16 @@
+"""
+Gokul Ramanan and Pranav Thamil
+9/18/25
+DS 4200
+"""
+
+def get_choolah_credit(*item):
+    choolah_prices = {"bowl": 11.99, "wrap": 10.49, "lassi": 5.75, "samosa": 4.99, "paneer": 2.49, "sprite": 1.99}
+    total_choolah = 0
+    for items in item:
+        if items.lower() in choolah_prices.keys():
+            total_choolah += choolah_prices[items.lower()]
+    return round(total_choolah, 2)
+
+choolah_bill = get_choolah_credit("Bowl", "LASSI", "samosa")
+print(f"The total Choolah bill today is: ${choolah_bill}")
